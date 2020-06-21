42e35135d4f5aef7ec767c014c35b95c0329e8ac
Valerie Fuchs <valerie@fuchsbs.de>
2020-06-21 15:42:24 +0200

    Added basic styling

--- APP/components/MainApp.js
+++ APP/components/MainApp.js
@@ -1,27 +1,55 @@
-import { getAllEntries } from '../lib/helpers.js';
+import { getAllEntries } from '../lib/storyblokCalls.js';
 
-/** Register the Main App element */
+/** Main App Element **/
 
 export class MainApp extends HTMLElement {
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
-		this.data = null;
-		this.isLoading = true;
+		this._data = null;
+		this._isLoading = true;
 		this._button = null;
 	}
 
 	async fetchData() {
 		this.loading = true;
 		const response = await getAllEntries();
-		this.data = response;
-		this.isLoading = false;
+		this._data = response;
+		this._isLoading = false;
 	}
 
 	appendStyles() {
 		const style = document.createElement('style');
 		style.textContent = `
+			.layout-container {
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+				width: 100%;
+				min-height: 100vh;
+				font-family: 'IBM Plex Sans', sans-serif;
+			}
+			.wrapper {
+				width: 60%;
+				border: solid 2px blue;
+				padding: 40px;
+			}
+			p {
+				width: 60%;
+				margin-top: 30px;
+				margin-bottom: 30px;
+			}
 			button {
+				border: solid 2px blue;
+				background: white;
+				color: blue;
+				padding: 10px;
+				font-size: 18px;
+				margin-top: 20px;
+				cursor: pointer;
+			}
+			button:hover {
 				background: blue;
 				color: white;
 			}
@@ -37,24 +65,28 @@ export class MainApp extends HTMLElement {
 	}
 
 	render() {
-		if (this.isLoading) {
+		if (this._isLoading) {
 			this.shadowRoot.innerHTML = `<div>Loading...</div>`;
 		} else {
 			this.shadowRoot.innerHTML = `
-				<h2>Storyblok Entries</h2>
-				<p>
-					This is a very basic example to show how to use Storyblok
-					with Custom Elements (Web Components). Below I am rendering
-					out the names of Storyblok entries coming from a Test Space.
-				</p>
-				<ul>
-					${this.data
-						.map((entry) => {
-							return `<li>${entry.name}</li>`;
-						})
-						.join('')}
-				</ul>
-				<button>Toggle data</button>
+				<div class="layout-container">
+					<div class="wrapper">
+						<h2>Storyblok x Web Components</h2>
+						<p>
+							This is a very basic example to show how to use Storyblok
+							with Custom Elements (Web Components). Below I am rendering
+							out the names of Storyblok entries coming from a Test Space.
+						</p>
+						<ul>
+							${this._data
+								.map((entry) => {
+									return `<li>${entry.name}</li>`;
+								})
+								.join('')}
+						</ul>
+						<button>⟶ Toggle data</button>
+					</div>
+				</div>
 			`;
 		}
 	}
