712a3514fa1ac0559a063918888623d0e3948fc9
Valerie Fuchs <valerie@fuchsbs.de>
2020-06-21 15:16:32 +0200

    Added styling logic

--- APP/components/MainApp.js
+++ APP/components/MainApp.js
@@ -8,6 +8,7 @@ export class MainApp extends HTMLElement {
 		this.attachShadow({ mode: 'open' });
 		this.data = null;
 		this.isLoading = true;
+		this._button = null;
 	}
 
 	async fetchData() {
@@ -17,12 +18,35 @@ export class MainApp extends HTMLElement {
 		this.isLoading = false;
 	}
 
+	appendStyles() {
+		const style = document.createElement('style');
+		style.textContent = `
+			button {
+				background: blue;
+				color: white;
+			}
+		`;
+		this.shadowRoot.appendChild(style);
+	}
+
+	appendEventListeners() {
+		this._button = this.shadowRoot.querySelector('button');
+		this._button.addEventListener('click', function() {
+			console.log('click');
+		});
+	}
+
 	render() {
 		if (this.isLoading) {
 			this.shadowRoot.innerHTML = `<div>Loading...</div>`;
 		} else {
 			this.shadowRoot.innerHTML = `
 				<h2>Storyblok Entries</h2>
+				<p>
+					This is a very basic example to show how to use Storyblok
+					with Custom Elements (Web Components). Below I am rendering
+					out the names of Storyblok entries coming from a Test Space.
+				</p>
 				<ul>
 					${this.data
 						.map((entry) => {
@@ -30,6 +54,7 @@ export class MainApp extends HTMLElement {
 						})
 						.join('')}
 				</ul>
+				<button>Toggle data</button>
 			`;
 		}
 	}
@@ -37,6 +62,8 @@ export class MainApp extends HTMLElement {
 	async connectedCallback() {
 		await this.fetchData();
 		this.render();
+		this.appendStyles();
+		this.appendEventListeners();
 	}
 }
 
